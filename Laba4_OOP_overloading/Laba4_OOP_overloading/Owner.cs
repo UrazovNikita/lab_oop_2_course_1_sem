907a00ba5f81adb713e4eed1c2b520a1957c5cff
UrazovNikita <nekstohay@gmail.com>
2020-10-07 04:52:13 +0300

    .

---
+++ Laba4_OOP_overloading/Laba4_OOP_overloading/Owner.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Laba4_OOP_overloading
+{
+    public class Owner
+    {
+        int Id = 25676;
+        string Name = "UUU";
+        string UO = "GGGGG";
+
+        public Owner(int Id, string Name, string UO)
+        {
+            this.Id = Id;
+            this.Name = Name;
+            this.UO = UO;
+        }
+        public void GetInfo()
+        {
+            Console.WriteLine($"{Id},{Name},{UO}");
+        }
+
+    }
+}
